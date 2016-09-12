7fce021580bb68449d5712be03ba4f86d4a047d3
Nathan <thaonk26@uww.edu>
2016-09-12 07:25:11 -0500

    initial upload

---
+++ LinqExamples/FunWithFunctions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExamples
+{
+    class FunWithFunctions
+    {
+        public delegate int AverageNumbers(string numbers);
+        //public delegate string AddFrequency(string n);
+        //public AddFrequency xx;
+        public AverageNumbers num;
+
+
+        public int AddNum(string z)
+        {
+            List<int> templist = new List<int>();
+            int total = 0;
+
+            foreach (var s in z.Split(','))
+            {
+                int temp;
+                if (int.TryParse(s, out temp))
+                {
+                    templist.Add(temp);
+                }
+            }
+            templist.Sort();
+            total = (int)templist.Skip(3).Average();
+            return total;
+        }
+        //public string Frequency(string r)
+        //{
+        //    int count = 1;
+        //    string name = r;
+
+        //    for (int i = 0; i < r.Length; i++)
+        //    {
+        //        int temp = 0;
+        //        for (int z = 0; z < r.Length; z++)
+        //        {
+        //            if (r[i].ToString().ToUpper() == r[z].ToString().ToUpper())
+        //            {
+        //                temp++;
+        //            }
+        //        }
+        //        name = name.Insert(i + count, temp.ToString());
+        //        count++;
+        //    }
+
+        //    return name;
+        //}
+    }
+}
---
+++ LinqExamples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string moreNumbers = "2,5,9,4,64,876,34,79,95,1";
+            string name = "Llewellyn";
+            FunWithFunctions haha = new FunWithFunctions();
+
+            haha.num = (string x) => { return haha.AddNum(x); };
+            Console.WriteLine(haha.num(moreNumbers));
+            //haha.xx = (string r) => { return haha.Frequency(r); };
+            //Console.WriteLine(haha.xx(name).ToString());
+            var counts = name.ToLower().GroupBy(c => c).Select(g => new { Letter = g.Key, Count = g.Count() });
+            
+            foreach (var item in counts)
+                Console.WriteLine("{0}{1}", item.Letter, item.Count);
+            Console.ReadLine();
+
+        }
+
+    }
+}
